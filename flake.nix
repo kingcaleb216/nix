9d1089051baab7119a97b29534fca6d98be40369
Caleb King <kingcaleb216@gmail.com>
2025-04-03 06:50:58 -0500

    Fixing bad flake

--- flake.nix
+++ flake.nix
@@ -19,101 +19,103 @@
       config.allowUnfree = true;
     };
   in {
-    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
+    nixosConfigurations = {
+      laptop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
 
-        # System configuration
-        ({ config, pkgs, ... }: {
-          imports = [ ./hardware-configuration.nix ];
+          # System configuration
+          ({ config, pkgs, ... }: {
+            imports = [ ./hardware-configuration.nix ];
 
-          networking.hostName = "laptop";
-          time.timeZone = "America/New_York";
-          i18n.defaultLocale = "en_US.UTF-8";
-          console.keyMap = "us";
+            networking.hostName = "laptop";
+            time.timeZone = "America/New_York";
+            i18n.defaultLocale = "en_US.UTF-8";
+            console.keyMap = "us";
 
-          users.users.caleb = {
-            isNormalUser = true;
-            extraGroups = [ "wheel" "networkmanager" "video" ];
-            shell = pkgs.zsh;
-          };
+            users.users.caleb = {
+              isNormalUser = true;
+              extraGroups = [ "wheel" "networkmanager" "video" ];
+              shell = pkgs.zsh;
+            };
 
-          environment.systemPackages = with pkgs; [
-            git zsh vim wget curl unzip zip htop
-            hyprland kitty waybar rofi
-            pipewire wireplumber pavucontrol
-            xdg-utils xdg-desktop-portal xdg-desktop-portal-hyprland
-            mesa vulkan-loader
-            fontconfig dejavu_fonts
-            ly fastfetch
-          ];
-
-          services.xserver.enable = true;
-          services.xserver.displayManager.ly.enable = true;
-
-          programs.zsh.enable = true;
-
-          sound.enable = true;
-          hardware.pulseaudio.enable = false;
-          services.pipewire = {
-            enable = true;
-            audio.enable = true;
-            pulse.enable = true;
-          };
-
-          fonts.packages = with pkgs; [
-            dejavu_fonts
-            nerdfonts
-          ];
-
-          system.stateVersion = "24.05";
-        }),
-
-        # Home Manager module
-        home-manager.nixosModules.home-manager,
-
-        # Home configuration for user caleb
-        ({ config, pkgs, ... }: {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.caleb = {
-            home.username = "caleb";
-            home.homeDirectory = "/home/caleb";
-
-            home.stateVersion = "24.05";
-
-            home.packages = with pkgs; [
-              starship zoxide bat fzf eza neovim
+            environment.systemPackages = with pkgs; [
+              git zsh vim wget curl unzip zip htop
+              hyprland kitty waybar rofi
+              pipewire wireplumber pavucontrol
+              xdg-utils xdg-desktop-portal xdg-desktop-portal-hyprland
+              mesa vulkan-loader
+              fontconfig dejavu_fonts
+              ly fastfetch
             ];
 
-            programs.zsh = {
+            services.xserver.enable = true;
+            services.xserver.displayManager.ly.enable = true;
+
+            programs.zsh.enable = true;
+
+            sound.enable = true;
+            hardware.pulseaudio.enable = false;
+            services.pipewire = {
               enable = true;
-              oh-my-zsh.enable = true;
-              oh-my-zsh.theme = "agnoster";
-              oh-my-zsh.plugins = [ "git" "zoxide" "fzf" ];
-              shellAliases = {
-                ll = "eza -la";
-                gs = "git status";
+              audio.enable = true;
+              pulse.enable = true;
+            };
+
+            fonts.packages = with pkgs; [
+              dejavu_fonts
+              nerdfonts
+            ];
+
+            system.stateVersion = "24.05";
+          }),
+
+          # Home Manager module
+          home-manager.nixosModules.home-manager,
+
+          # Home configuration for user caleb
+          ({ config, pkgs, ... }: {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.caleb = {
+              home.username = "caleb";
+              home.homeDirectory = "/home/caleb";
+
+              home.stateVersion = "24.05";
+
+              home.packages = with pkgs; [
+                starship zoxide bat fzf eza neovim
+              ];
+
+              programs.zsh = {
+                enable = true;
+                oh-my-zsh.enable = true;
+                oh-my-zsh.theme = "agnoster";
+                oh-my-zsh.plugins = [ "git" "zoxide" "fzf" ];
+                shellAliases = {
+                  ll = "eza -la";
+                  gs = "git status";
+                };
               };
+
+              programs.kitty.enable = true;
+
+              xdg.configFile = {
+                "hypr".source = "/opt/repos/hyprland/hypr";
+                "kitty/kitty.conf".source = "/opt/repos/hyprland/kitty/kitty.conf";
+                "waybar/config".source = "/opt/repos/hyprland/waybar/config";
+                "waybar/style.css".source = "/opt/repos/hyprland/waybar/style.css";
+              };
+
+              home.file.".config/wall.png".source = "/opt/repos/hyprland/wall.png";
+
+              home.activation.applyTheme = config.lib.hm.dag.entryAfter ["writeBoundary"] ''
+                /opt/repos/hyprland/theme.sh
+              '';
             };
-
-            programs.kitty.enable = true;
-
-            xdg.configFile = {
-              "hypr".source = "/opt/repos/hyprland/hypr";
-              "kitty/kitty.conf".source = "/opt/repos/hyprland/kitty/kitty.conf";
-              "waybar/config".source = "/opt/repos/hyprland/waybar/config";
-              "waybar/style.css".source = "/opt/repos/hyprland/waybar/style.css";
-            };
-
-            home.file.".config/wall.png".source = "/opt/repos/hyprland/wall.png";
-
-            home.activation.applyTheme = config.lib.hm.dag.entryAfter ["writeBoundary"] ''
-              /opt/repos/hyprland/theme.sh
-            '';
-          };
-        })
-      ];
+          })
+        ];
+      };
     };
   }
 
